4458b3556f6cb112c5e972cfcae2119d0848a577
akoh1 <samuela@ehealth4everyone.com>
2019-03-13 19:32:49 +0100

    add requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,6 @@ from . import views
 urlpatterns = [
     path('', views.Tasks.as_view()),
     # path('<int:pk>/', views.DetailTodo.as_view()),
-    path('login/', views.login),
+    # path('login/', views.login),
     path('rest-auth/', include('rest_auth.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -19,18 +19,18 @@ class Tasks(generics.ListCreateAPIView):
     queryset = Tasks.objects.all()
     serializer_class = TaskSerializer
 
-def login(request):
-    # username = request.data.get("username")
-    # password = request.data.get("password")
-    username = request.data["username"]
-    password = request.data["password"]
-    if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
-                        status=HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if not user:
-        return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
-    token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key},
-                    status=HTTP_200_OK)    
+# def login(request):
+#     # username = request.data.get("username")
+#     # password = request.data.get("password")
+#     username = request.data["username"]
+#     password = request.data["password"]
+#     if username is None or password is None:
+#         return Response({'error': 'Please provide both username and password'},
+#                         status=HTTP_400_BAD_REQUEST)
+#     user = authenticate(username=username, password=password)
+#     if not user:
+#         return Response({'error': 'Invalid Credentials'},
+#                         status=HTTP_404_NOT_FOUND)
+#     token, _ = Token.objects.get_or_create(user=user)
+#     return Response({'token': token.key},
+#                     status=HTTP_200_OK)    
